ec730f5bca7a909c97c48469057f701bf42e99b0
Danielyan86 <516495459@qq.com>
2018-08-31 17:33:46 +0800

    Update leetcode

--- 动态规划-dynamic_programming/Unique Paths II.py
+++ 动态规划-dynamic_programming/Unique Paths II.py
@@ -8,51 +8,69 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid):
         """
-        :type obstacleGrid: List[List[int]]
+        :type Grid: List[List[int]]
         :rtype: int
         """
-
-        lines, cols = len(obstacleGrid), len(obstacleGrid[0])
-        if obstacleGrid[lines - 1][cols - 1] == 1 or obstacleGrid[0][0] == 1:  # 如果终点或者起点为障碍，则永远无法到达
+        Grid = obstacleGrid
+        lines, cols = len(Grid), len(Grid[0])
+        if Grid[lines - 1][cols - 1] == 1 or Grid[0][0] == 1:  # 如果终点或者起点为障碍，则永远无法到达
             return 0
         # 因为数字1需要用来计算路径，所以先把表示障碍的1更新为-1, 并初始化边界值
         if lines == 1 and cols == 1:
-            return 1 if obstacleGrid[lines - 1][cols - 1] == 0 else 0
-        for line, line_list in enumerate(obstacleGrid):
+            return 1 if Grid[lines - 1][cols - 1] == 0 else 0
+        for line, line_list in enumerate(Grid):
             for col, num in enumerate(line_list):
                 if num == 1:
-                    obstacleGrid[line][col] = -1
-        obstacleGrid[0][0] = 1
+                    Grid[line][col] = -1
+        Grid[0][0] = 1
 
         # 初始化第一行边界值，如果遇到-1，则-1后面无法到达，因此停止复制，保持为0
-        for index, num in enumerate(obstacleGrid[0]):
+        for index, num in enumerate(Grid[0]):
             if num != -1:
-                obstacleGrid[0][index] = 1
+                Grid[0][index] = 1
             else:
                 break
         # 初始化第一列边界值，如果遇到-1，则-1后面无法到达，因此为0
-        for line, line_list in enumerate(obstacleGrid):
+        for line, line_list in enumerate(Grid):
             if line_list[0] != -1:
-                obstacleGrid[line][0] = 1
+                Grid[line][0] = 1
             else:
                 break
 
         for line_index in range(1, lines):
             for col_index in range(1, cols):
-                if obstacleGrid[line_index][col_index] != -1:
-                    if obstacleGrid[line_index - 1][col_index] == -1 and obstacleGrid[line_index][col_index - 1] == -1:
+                if Grid[line_index][col_index] != -1:
+                    if Grid[line_index - 1][col_index] == -1 and Grid[line_index][col_index - 1] == -1:
                         continue
-                    elif obstacleGrid[line_index - 1][col_index] == -1 or obstacleGrid[line_index][col_index - 1] == -1:
-                        obstacleGrid[line_index][col_index] = max(obstacleGrid[line_index - 1][col_index],
-                                                                  obstacleGrid[line_index][col_index - 1])
+                    elif Grid[line_index - 1][col_index] == -1 or Grid[line_index][col_index - 1] == -1:
+                        Grid[line_index][col_index] = max(Grid[line_index - 1][col_index],
+                                                          Grid[line_index][col_index - 1])
                     else:
-                        obstacleGrid[line_index][col_index] = obstacleGrid[line_index - 1][col_index] + \
-                                                              obstacleGrid[line_index][col_index - 1]
-        return obstacleGrid[lines - 1][cols - 1]
+                        Grid[line_index][col_index] = Grid[line_index - 1][col_index] + \
+                                                      Grid[line_index][col_index - 1]
+        return Grid[lines - 1][cols - 1]
+
+
+class Solution2:
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        """
+        :type obstacleGrid: List[List[int]]
+        :rtype: int
+        """
+        res = [0] * len(obstacleGrid[0])  # m x n matrix
+        res[0] = 1
+        for i in obstacleGrid:
+            for j, each in enumerate(i):
+                if each == 0:
+                    if j != 0:
+                        res[j] += res[j - 1]
+                else:
+                    res[j] = 0
+        return res[-1]
 
 
 if __name__ == '__main__':
     test_data = [[1, 0]]
-    s_obj = Solution()
+    s_obj = Solution2()
     res = s_obj.uniquePathsWithObstacles(test_data)
     print(res)
