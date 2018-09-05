d3de3d6f9825cfa0243832b45ceffe7bf6f826ab
Danielyan86 <516495459@qq.com>
2018-09-05 17:26:21 +0800

    update leetcode

--- 动态规划-dynamic_programming/Dungeon Game.py
+++ 动态规划-dynamic_programming/Dungeon Game.py
@@ -1,7 +1,8 @@
 # 题目 https://leetcode-cn.com/problems/dungeon-game/description/
 
 # 分析 刚开始时候还是想的是从左上到右下的顺序思维，然后不断更新初始值，
-# 这题可以采用逆向思维，即从出口反推回去
+# 这题应该采用逆向思维，即从出口反推回去，因为限定条件是出来时候为1，所以初始值是确定的，如果从入口开始，则初始值是不确定的，
+# 有点像解方程，因为计算机不会自动解方程，需要用逆向思维反推回去
 # 要注意再任意时刻血量不能低于0，即和为正数，要起始值最低，即每一步选择和为最大值
 
 class Solution:
@@ -16,16 +17,49 @@ class Solution:
         DP[-1] = 1  # 退出时候值为1，
 
         # 从最后一个格子开始，反向遍历二维数组
-        for i in reversed(range(len(dungeon))):
-            DP[-1] = max(DP[-1] - dungeon[i][-1], 1)  # 更新最后一个数字
-            for j in reversed(range(len(dungeon[i]) - 1)):
-                min_HP_on_exit = min(DP[j], DP[j + 1])
-                DP[j] = max(min_HP_on_exit - dungeon[i][j], 1)
+        for row_idx in reversed(range(len(dungeon))):
+            # 顺序过来每到一个格子是+，反着回去就是-，如果最后一个格子为正数，则取最后一个格子的值，如果为0或者负数，则取1，因为每个时候状态需要大于1
+            DP[-1] = max(DP[-1] - dungeon[row_idx][-1], 1)
+            for col_idx in reversed(range(len(dungeon[row_idx]) - 1)):  # 最后一列倒数第二个格子开始，初始化最后一列
+                min_HP_on_exit = min(DP[col_idx], DP[col_idx + 1])  # 更新退出血量
+                # 如果是一个负数，则加上一个血量，因为。如果是正数，则减去。如果减去一个正数小于1了，证明这个格子加血量已经够了，则更新为1
+                DP[col_idx] = max(min_HP_on_exit - dungeon[row_idx][col_idx], 1)
 
         return DP[0]
 
 
+class Solution2:
+    def calculateMinimumHP(self, dungeon):
+        """
+        :type dungeon: List[List[int]]
+        :rtype: int
+        """
+        rows, cols = len(dungeon), len(dungeon[0]),
+        if cols == 1:
+            return 0 if dungeon[0][0] > 0 else abs(dungeon[0][0] + 1)
+        else:
+            # min_HP_on_exit = 1
+            dungeon[rows - 1][cols - 1] = 1 if 1 - dungeon[rows - 1][cols - 1] < 0 else 1 - dungeon[rows - 1][cols - 1]
+            # 遍历二维数组，根据规则，到达每一个格子应该为正数
+            for row in reversed(range(rows)):
+                for col in reversed(range(cols)):
+                    if row == rows - 1 and col == cols - 1:
+                        continue
+                    elif row == rows - 1:  # 初始化最后一行
+                        dungeon[row][col] = max(dungeon[row][col + 1] - dungeon[row][col], 1)
+                    elif col == cols - 1:  # 初始化最后一列
+                        dungeon[row][col] = max(dungeon[row + 1][col] - dungeon[row][col], 1)
+                    else:
+                        dungeon[row][col] = max(min(dungeon[row + 1][col] - dungeon[row][col],
+                                                    dungeon[row][col + 1] - dungeon[row][col]), 1)
+            return dungeon[0][0]
+
+
 if __name__ == '__main__':
+    test_data = [[0]]
     s_obj = Solution()
-    res = s_obj.calculateMinimumHP([[-2, -3, 3], [-5, -10, 1], [10, 30, -5]])
+    res = s_obj.calculateMinimumHP(test_data)
+    print(res)
+    s_obj = Solution2()
+    res = s_obj.calculateMinimumHP(test_data)
     print(res)
