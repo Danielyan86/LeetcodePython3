d96c08d3413046bb0de5633dd234f628f0d0f08e
Danielyan86 <516495459@qq.com>
2018-09-10 11:31:39 +0800

    update leetcode

--- Longest_Palindromic_Substring.py
+++ Longest_Palindromic_Substring.py
@@ -1,9 +1,11 @@
+# 题目：给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为1000。
 class Solution:
     def longestPalindrome(self, s):
         """
         :type s: str
         :rtype: str
         """
+        # 思路：
         if len(s) > 0:
             if s == s[::-1]:
                 return s
@@ -12,9 +14,11 @@ class Solution:
                     sub_str = s[j:j + i]
                     if sub_str == sub_str[::-1]:
                         return sub_str
+        else:
+            return ""
 
 
 if __name__ == '__main__':
     s_obj = Solution()
-    res = s_obj.longestPalindrome("abcda")
+    res = s_obj.longestPalindrome("abcba")
     print(res)
