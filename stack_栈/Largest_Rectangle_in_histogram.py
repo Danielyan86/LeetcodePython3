8249f6a48366e13256dc6682e309f577eda46e2c
Danielyan86 <516495459@qq.com>
2018-11-12 13:45:16 +0800

    update leetcode

--- stack_栈/Largest_Rectangle_in_histogram.py
+++ stack_栈/Largest_Rectangle_in_histogram.py
@@ -1,5 +1,6 @@
 # 给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1
 # 求在该柱状图中，能够勾勒出来的矩形的最大面积。
+
 class Solution:
     def largestRectangleArea(self, heights: list) -> int:
         """
@@ -18,6 +19,19 @@ class Solution:
         heights.pop()
         return max_area
 
+    def _increase_list(self, heights: list) -> int:
+        # [1, 3, 4, 7]
+        stack_index = [index for index in range(len(heights))]
+        max_area = 0
+        top_index = len(stack_index)
+        while stack_index:
+            index = stack_index.pop()
+            width = top_index - index
+            area = heights[index] * width
+            print(area)
+            max_area = max(max_area, area)
+        return max_area
+
 
 class Solution2:
     def largestRectangleArea(self, heights: list) -> int:
@@ -25,23 +39,25 @@ class Solution2:
         :type heights: List[int]
         :rtype: int
         """
-        heights.append(-1)
-        index, max_area = 0, 0
-        index_stack = []
+        heights.append(-1)  # 末尾添加一个-1，方便我们用索引值相减，求出长方形的宽。假设最后一个柱子为最大的单个柱子,索引值为index，则宽度为len(height)-index
+        max_area = 0
+        stack_index = []
+        # 索引值入栈
+        index = 0
         while index < len(heights):
-            # 若高度为升序,或者索引栈为空，索引值则入栈，不计算面积
-            if len(index_stack) == 0 or heights[index_stack[-1]] <= heights[index]:
-                index_stack.append(index)
+            if stack_index:  # 如果不为空，判断是否升序，如果升序，则入栈
+                if heights[index] >= heights[stack_index[-1]]:
+                    stack_index.append(index)
+                    index = index + 1
+                else:  # 否则开始计算，直到升序
+                    previous_index = stack_index.pop()
+                    if stack_index:
+                        max_area = max(max_area, (index - stack_index[-1] - 1) * heights[previous_index])
+                    else:
+                        max_area = max(max_area, index * heights[previous_index])
+            else:  # 如果为空，则入栈
+                stack_index.append(index)
                 index = index + 1
-            else:
-                # 若开始降序，求局部升序柱子的最大面积
-                top = index_stack.pop()  # 若高度开始降序，则把最后一个出栈，也就是上一个最高的柱子序号，每次弹出一个最高柱子，直到为空
-                if len(index_stack) == 0:  # 如果栈为空，10有一个柱子，面积为一个柱子的高度，2）降序排列情况，则长方形高度为当前值
-                    area = heights[top] * index
-                else:  # 如果不为空，先求刚刚弹出的最高柱子的面积，然后求次高柱子和最高柱子的最大面积，以此类推
-                    # 因为是升序排列，所以次高柱子就是我们每次求的长方形的高度
-                    area = heights[top] * (index - index_stack[-1] - 1)
-                max_area = max(max_area, area)
         return max_area
 
 
@@ -51,6 +67,7 @@ def test_solution():
     assert s_obj.largestRectangleArea([1, 3, 1]) == 3
     assert s_obj.largestRectangleArea([1, 3, 1, 1]) == 4
     assert s_obj.largestRectangleArea([3, 2, 1]) == 4
+    assert s_obj.largestRectangleArea([2, 0, 2]) == 2
 
 
 def test_solution2():
@@ -59,9 +76,13 @@ def test_solution2():
     assert s_obj.largestRectangleArea([1, 3, 1]) == 3
     assert s_obj.largestRectangleArea([1, 3, 1, 1]) == 4
     assert s_obj.largestRectangleArea([3, 2, 1]) == 4
+    assert s_obj.largestRectangleArea([2, 0, 2]) == 2
+    assert s_obj.largestRectangleArea([0, 3, 2, 5]) == 6
 
 
 if __name__ == '__main__':
     s_obj = Solution2()
-    res = s_obj.largestRectangleArea([1, 3, 4, 2])
+    # s_obj._increase_list([1, 3, 4, 7])
+    # s_obj._descrea_list([4, 3, 2, 1])
+    res = s_obj.largestRectangleArea([0, 3, 2, 5])
     print(res)
