b3520597cafa0533a838128c0b0165bf048fd113
Danielyan86 <516495459@qq.com>
2018-12-10 14:13:58 +0800

    update leetcode

--- stack_栈/Binary_Tree_Preorder_Traversal.py
+++ stack_栈/Binary_Tree_Preorder_Traversal.py
@@ -1,7 +1,7 @@
 # Definition for a binary tree node.
 # 题目：给定一个二叉树，返回它的 前序 遍历。
 # 解题思路，尝试用迭代法遍历。把节点存入栈数据结构
-# 难点：刚开始把判定条件写成当前节点左右节点为空，把问题复杂花了，从一开始逻辑搞复杂了，直接判断当前节点为空不就行了么！
+# 难点：合理利用两个while循环
 class TreeNode:
     def __init__(self, x):
         self.val = x
@@ -15,32 +15,18 @@ class Solution:
         :type root: TreeNode
         :rtype: List[int]
         """
-
         number_list = []
         if not root:
             return number_list
         nodes_stack = []
         current_node = root
         while current_node or nodes_stack:
-              # 栈应该是存上一个节点，第一次是根节点
-            while current_node.left:
-                nodes_stack.append(current_node)
+            while current_node:  # 左节点为none，跳出第二层循环
+                nodes_stack.append(current_node)  # 节点存在，则入栈
                 number_list.append(current_node.val)
-                current_node = current_node.left
-            number_list.append(current_node.val)
-
-            if nodes_stack:  # 变换方向
-                while nodes_stack:
-                    previous_node = nodes_stack.pop()
-                    current_node = previous_node.right
-                    if current_node:
-                        break
-                if current_node:
-                    nodes_stack.append(current_node)
-                    number_list.append(current_node.val)
-            else:
-                nodes_stack.append(current_node)
-                number_list.append(current_node.val)
+                current_node = current_node.left  # 寻找左节点
+            last_node = nodes_stack.pop()  # 寻找右 节点
+            current_node = last_node.right  # 即使右节点为none，还是给当前值，这样不会再次进入寻找左节点的第二层循环，继续弹出栈的最后一个值
         return number_list
 
 
