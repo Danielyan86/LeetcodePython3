a150d7c52cf7dfe21fc4d7175ba343a75b1ed43d
Danielyan86 <516495459@qq.com>
2021-08-22 22:49:34 +0800

    add new one

---
+++ Tree/101_symmetric_tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        if root is None:
+            return True
+        return self.isSysmetriRecu(root.left, root.right)
+
+    def isSysmetriRecu(self, left, right):
+        if left is None and right is None:
+            return True
+        if left is None or right is None or left.val != right.val:
+            return False
+        return self.isSysmetriRecu(left.left, right.right) and self.isSysmetriRecu(left.right, right.left)
