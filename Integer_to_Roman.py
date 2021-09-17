a8e426c0dbdfa3be217c24236780863e40ec4d9c
Danielyan86 <516495459@qq.com>
2021-09-17 20:13:02 +0800

    update

--- Integer_to_Roman.py
+++ Integer_to_Roman.py
@@ -9,13 +9,13 @@ class Solution:
         # Roman_dic = {1: "I", 5: "V", 10: "X", 50: "L", 100: "C", 500: "D", 1000: "M"}
 
         rome_str = ""
-        lable = 1
-        if 0 < num < 4000:
+        label = 1
+        if num > 0 and num < 4000:
             while num != 0:
                 res = num % 10
-                rome_str = self.convert_num_into_rome_letter(res, lable) + rome_str
+                rome_str = self.convert_num_into_rome_letter(res, label) + rome_str
                 num = num // 10
-                lable = lable + 1
+                label = label + 1
         print(rome_str)
         return rome_str
 
