822b3297dc76f56a85ecc1d022af96c7a40355b1
Danielyan86 <516495459@qq.com>
2023-09-12 23:41:08 +0800

    ADD GITIGNORE FILE

--- LeetCode/4_median_number.py
+++ LeetCode/4_median_number.py
@@ -13,8 +13,20 @@ class Solution:
         else:
             return new_list[length // 2]
 
+    def findMedianSortedArrays2(self, nums1, nums2):
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+        len1, len2 = len(nums1), len(nums2)
+        imin, imax, half_len = 0, len1, (len1 + len2 + 1) // 2
 
-if __name__ == '__main__':
+        while imin <= imax:
+            i = (imin + imax) // 2
+            j = half_len - i
+
+            if i<len1 and
+
+
+if __name__ == "__main__":
     s_obj = Solution()
     res = s_obj.findMedianSortedArrays([1, 2], [3, 4])
     print(res)
