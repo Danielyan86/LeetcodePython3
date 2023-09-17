56eefe1ad0175d0215de56174ef51d4231293f71
Danielyan86 <516495459@qq.com>
2023-09-17 12:37:36 +0800

    update

--- Container_With_Most_Water.py
+++ Container_With_Most_Water.py
@@ -11,7 +11,8 @@ class Solution:
             for i in range(0, length - 1):
                 for j in range(i + 1, length):
                     hight_min = min(height[i], height[j])
-                    max_water_container = max(max_water_container, hight_min * (j - i))
+                    max_water_container = max(
+                        max_water_container, hight_min * (j - i))
             return max_water_container
         else:
             return 0
@@ -30,6 +31,23 @@ class Solution:
                 else:
                     right_pointer = right_pointer - 1
         return max_area
+
+    # 此题关键破题点是双指针怎么移动问题
+    # 无论左移还是右移，宽度都是减小，因此只能移动高度更短的一边，面积才可能增加,因为面积是由高度最低的木板决定的
+    def maxArea2(self, height: List[int]) -> int:
+        # 但凡用min，max这一类函数，都需要一个初始值,最小或者无限大
+        if len(height) < 2:
+            return None
+        maxArea = 0
+        point_l, point_r = 0, len(height) - 1
+        while point_l < point_r:
+            temp_area = min(height[point_l], height[point_r]) * (point_r - point_l)
+            maxArea = max(temp_area, maxArea)
+            if height[point_l] <= height[point_r]:
+                point_l = point_l + 1
+            else:
+                point_r = point_r - 1
+        return maxArea
 
 
 if __name__ == '__main__':
