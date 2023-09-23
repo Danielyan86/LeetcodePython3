595bcbdb9ae7ffaececb0befa317d97a03a526b6
Danielyan86 <516495459@qq.com>
2023-09-23 23:13:30 +0800

    Update

--- Tree/94_binary_tree_innorder_traversal.py
+++ Tree/94_binary_tree_innorder_traversal.py
@@ -11,19 +11,29 @@ class TreeNode:
 
 
 class Solution:
+    # 使用栈的数据结构解决这个问题的一个原因是binary tree的travseral order和处理数据oder不一致
+    # 先travseral后处理，刚好符合stack的特性
     def inorderTraversal(self, root: TreeNode) -> List[int]:
+        # 当根节点不为空或者栈不为空，从根节点开始
+        # 若有左节点，则一直压栈
+        # 若无左节点，则弹出，访问右节点
+        if not root:
+            return []
 
-        result = list()  # 存储最后结果
-        if root is None:
-            return result
+        stack = []
+        res = []
 
-        stack = list()  # 栈
-        while stack or root:
-            if root is not None:  # root不为空则入栈
+        while root or stack:
+            # 一直向左子树走，每一次将当前节点保存到栈中
+            # should be notice that the current node is None can go into else branch
+            # None node is like 辅助节点，类似LRU双向链表的头尾辅助节点
+            if root:
                 stack.append(root)
                 root = root.left
+            # 当前节点为空，证明走到了最左边，从栈中弹出节点加入结果数组
+            # 开始对右子树重复上述过程。
             else:
                 root = stack.pop()
-                result.append(root.val)
+                res.append(root.val)
                 root = root.right
-        return result
+        return res
