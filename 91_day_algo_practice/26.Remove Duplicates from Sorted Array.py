3dbb25585f18816de48bebc52a4190d7778df3d3
Danielyan86 <516495459@qq.com>
2023-12-11 10:12:56 +0800

    update

---
+++ 91_day_algo_practice/26.Remove Duplicates from Sorted Array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums) == 1: return 1
+        l = 0
+        for r in range(1, len(nums)):
+            if nums[r] != nums[l]:
+                nums[l + 1] = nums[r]
+                l += 1
+        return l + 1
---
+++ SQL/hackerrank_summary.md
@@ -0,0 +1,2 @@
+- take the remainder - MOD()
+- remove duplicate - distinct
