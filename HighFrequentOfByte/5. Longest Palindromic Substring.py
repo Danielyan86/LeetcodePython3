c044badf708cbfdc0bd60aa087a5b08f6929fd75
Danielyan86 <516495459@qq.com>
2024-01-11 19:42:02 +0800

    Update

--- HighFrequentOfByte/5. Longest Palindromic Substring.py
+++ HighFrequentOfByte/5. Longest Palindromic Substring.py
@@ -1,5 +1,11 @@
+#
 class Solution2(object):
-    # 单指针遍历
+    # 单指针遍历,从坐标0开始
+    # 每次只判断两个字符串
+    # 注意起始坐标要格外-1，是因为要大于当前最大的，所以要减一
+    # 一个是当前下标，一个是当前完后挪动一个
+    # 全部采用切片操作
+
     def longestPalindrome(self, s):
         res = ""
         for i in range(len(s)):
@@ -8,7 +14,7 @@ class Solution2(object):
             start = i - len(res) - 1 if i - len(res) - 1 >= 0 else 0
             temp = s[start : i + 1]
             # 如果一个字符串已经是回文，不用再判断
-            if temp == temp[::-1]:  # 分奇数和偶数两种情况，如果是偶数，else则是奇数，反之亦然
+            if temp == temp[::-1]:  # 分奇数和偶数两种情况，
                 res = temp
             else:
                 temp = temp[1:]
--- dynamic_programming/5_Longest_Palindromic_Substring.py
+++ dynamic_programming/5_Longest_Palindromic_Substring.py
@@ -23,64 +23,6 @@ class Solution0:
         return left + 1, right - 1
 
 
-class Solution:
-    def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        # 思路：长度从大到小遍历每一个子串，再反转字符[::-1]串做比较，此方法效率低下
-        if len(s) > 0:
-            if s == s[::-1]:
-                return s
-            for i in range(len(s) - 1, 0, -1):
-                for j in range(0, len(s) - i + 1):
-                    sub_str = s[j : j + i]
-                    if sub_str == sub_str[::-1]:
-                        return sub_str
-        else:
-            return ""
-
-
-class Solution2:
-    def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        # 用start和end来表示字符串起始和结束下标，可以用一个二位数字来表示是否为回文
-        # pa[start][end]=True 表示是回文
-        # 初始化pa[start][end],  start=end时候表示单个字母，则为true，
-        # 奇数个回文起点是start=end，偶数个回文起点是end = start +1
-        # 长度从小到大
-        s_len = len(s)
-        if s_len > 0:
-            longest_le, longest_str = 1, s[0]
-            if s == s[::-1]:
-                return s
-            # 初始化数组
-            pa = [[None for _ in range(s_len)] for _ in range(s_len)]
-            for i in range(s_len):
-                pa[i][i] = True
-                if i < s_len - 1:
-                    pa[i][i + 1] = True if s[i] == s[i + 1] else False
-                    if pa[i][i + 1]:
-                        longest_le, longest_str = 2, s[i : i + 2]
-
-            for start in reversed(range(s_len)):
-                for end in range(start + 1, s_len):
-                    if pa[start + 1][end - 1] and s[start] == s[end] and pa[start][end] == None:
-                        pa[start][end] = True
-                        if end - start + 1 > longest_le:
-                            longest_le, longest_str = (
-                                end - start + 1,
-                                s[start : end + 1],
-                            )
-            return longest_str
-        else:
-            return ""
-
-
 def test_solution2():
     s_obj = Solution2()
     assert s_obj.longestPalindrome("babad") == "aba"
