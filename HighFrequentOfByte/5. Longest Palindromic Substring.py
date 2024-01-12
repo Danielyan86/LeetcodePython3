d24c2cb869af56e8b68007d5a7a0851bcc6e9e0d
Danielyan86 <516495459@qq.com>
2024-01-12 17:12:02 +0800

    Update

--- HighFrequentOfByte/5. Longest Palindromic Substring.py
+++ HighFrequentOfByte/5. Longest Palindromic Substring.py
@@ -1,23 +1,19 @@
-#
-class Solution2(object):
-    # 单指针遍历,从坐标0开始
-    # 每次只判断两个字符串
-    # 注意起始坐标要格外-1，是因为要大于当前最大的，所以要减一
-    # 一个是当前下标，一个是当前完后挪动一个
-    # 全部采用切片操作
+# 单指针遍历,从坐标0开始
+# 每次只判断两个字符串
+# 注意起始坐标要格外-1，是因为要大于当前最大的，所以要减一
+# 一个是当前下标，一个是当前完后挪动一个
+# 全部采用切片操作
 
-    def longestPalindrome(self, s):
+
+class Solution:
+    # 不要采用取巧的切片操作，多用变量
+    def longestPalindrome(self, s: str) -> str:
         res = ""
         for i in range(len(s)):
-            # 如果起始下标小于0，则从0开始，
-            # 起始位置应该大于之前已经存在的最大回文串长度
-            start = i - len(res) - 1 if i - len(res) - 1 >= 0 else 0
-            temp = s[start : i + 1]
-            # 如果一个字符串已经是回文，不用再判断
-            if temp == temp[::-1]:  # 分奇数和偶数两种情况，
-                res = temp
-            else:
-                temp = temp[1:]
-                if temp == temp[::-1]:
-                    res = temp
+            start = i - len(res) - 1 if i - len(res) - 1 > 0 else 0
+            tmp1, tmp2 = s[start : i + 1], s[start + 1 : i + 1]
+            if tmp1 == tmp1[::-1]:
+                res = tmp1
+            elif tmp2 == tmp2[::-1]:
+                res = tmp2
         return res
