3789a11444c744b00dc95bf3ddc773f98e4af7cc
Danielyan86 <516495459@qq.com>
2024-01-19 09:57:36 +0800

    Update

--- HighFrequentOfByte/124. Binary Tree Maximum Path Sum.py
+++ HighFrequentOfByte/124. Binary Tree Maximum Path Sum.py
@@ -2,7 +2,8 @@
 # 后续遍历模版
 
 
-# 设置一个全局的max变量，注意每次max的值
+# 设置一个全局的max变量，当max_sum 在函数内被作为参数使用时候，需要注意用nonlocal声明是全局变量
+# 注意在左右节点返回计算结果时候，为什么为负数时候要取0？取0意义就是当前子节点返回值为负数，那么这样只会越加越小，不满足题目需求，起始也就是不用再去走当前子节点了。不用max函数更好理解一些
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
         max_sum = float("-inf")
@@ -12,9 +13,10 @@ class Solution:
                 return 0
             nonlocal max_sum
             # Calculate the maximum path sum in the left and right subtrees
-            left = max(0, dfs(node.left))
-            right = max(0, dfs(node.right))
-
+            left = dfs(node.left)
+            left = left if left >= 0 else 0
+            right = dfs(node.right)
+            right = right if right >= 0 else 0
             # Update the maximum path sum
             max_sum = max(max_sum, left + right + node.val)
 
