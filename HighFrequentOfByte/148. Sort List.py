7db21875f02ef3ce12fe58d06dc325b13ff56420
Danielyan86 <516495459@qq.com>
2024-01-21 15:11:53 +0800

    Update

--- HighFrequentOfByte/148. Sort List.py
+++ HighFrequentOfByte/148. Sort List.py
@@ -1,27 +1,32 @@
 # 分治思想，归并排序，先归类，也就是分成最小两个元素后，再开始合并有序列表
-# 需要特别注意的是，快慢指针法快指针起始位置要先走一步
+
+
+# 需要特别注意的是，快慢指针法中的快指针起始位置要先走一步
 # 否则只有两个节点时候就会出问题，慢指针将指向第二个节点，进入无限递归
+
+
+# 合并两个有序链表参考这个 Merge k Sorted Lists https://leetcode.cn/problems/merge-k-sorted-lists/description/
 class Solution:
-    def sortList(self, head: ListNode) -> ListNode:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if head is None or head.next is None:
             return head
-        sp, fp = head, head.next  # 快慢指针法寻找link中点
-        while fp and fp.next:
-            sp, fp = sp.next, fp.next.next
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        cur2_h = slow.next
+        slow.next = None  #  断开链表
+        cur1, cur2 = self.sortList(head), self.sortList(cur2_h)
 
-        mid, sp.next = sp.next, None
-        # recursive for cutting
-        left, right = self.sortList(head), self.sortList(mid)
-
-        # 合并两个有序链表
-        # Merge k Sorted Lists https://leetcode.cn/problems/merge-k-sorted-lists/description/
-        cur = dummy_h = ListNode(0)
-        while left and right:
-            if left.val < right.val:
-                cur.next, left = left, left.next
+        # 合并链表
+        cur = dummy = ListNode(0)
+        while cur1 and cur2:
+            if cur1.val < cur2.val:
+                cur.next = cur1
+                cur1 = cur1.next
             else:
-                cur.next, right = right, right.next
+                cur.next = cur2
+                cur2 = cur2.next
             cur = cur.next
-
-        cur.next = left if left else right
-        return dummy_h.next
+        cur.next = cur1 if cur1 else cur2
+        return dummy.next
--- HighFrequentOfByte/165. Compare Version Numbers.py
+++ HighFrequentOfByte/165. Compare Version Numbers.py
@@ -1,20 +1,18 @@
 # 逗号分割，转化成数字list
-# 根据长度补0
-# 取下标依次比较
+# 取最大长度为比较长度
+# 从左往右取下标依次比较
+# 当下标超出任意一个数组最大值时候取0比较
+# 如果不想等则提前结束
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
-        lst1 = [int(x) for x in version1.split(".")]
-        lst2 = [int(x) for x in version2.split(".")]
-
-        while len(lst1) < len(lst2):
-            lst1.append(0)
-        while len(lst2) < len(lst1):
-            lst2.append(0)
-        i = 0
-        while i < len(lst1):
-            if lst1[i] > lst2[i]:
-                return 1
-            elif lst1[i] < lst2[i]:
+        v1 = [int(x) for x in version1.split(".")]
+        v2 = [int(x) for x in version2.split(".")]
+        max_l = max(len(v1), len(v2))
+        for i in range(max_l):
+            n1 = 0 if i >= len(v1) else v1[i]
+            n2 = 0 if i >= len(v2) else v2[i]
+            if n1 < n2:
                 return -1
-            i += 1
+            if n1 > n2:
+                return 1
         return 0
