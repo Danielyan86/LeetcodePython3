d052aa1fde4609a5a08c71ce6fd94c7a70ecaa50
Danielyan86 <516495459@qq.com>
2024-07-18 21:42:40 +1200

    Update

--- HighFrequentOfByte/46. Permutations.py
+++ HighFrequentOfByte/46. Permutations.py
@@ -1,3 +1,19 @@
+# Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+
+# Example 1:
+
+# Input: nums = [1,2,3]
+# Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+# Example 2:
+
+# Input: nums = [0,1]
+# Output: [[0,1],[1,0]]
+# Example 3:
+
+# Input: nums = [1]
+# Output: [[1]]
+
 # 正向思维，从0个元素开始加，这种方法最容写，没有格外多余的步骤，也符合人的排序思维
 # 一开始就通过for循环分成了n叉树
 # 在每一个子树里面遍历全局的列表，如果元素已经存在了，则跳过
