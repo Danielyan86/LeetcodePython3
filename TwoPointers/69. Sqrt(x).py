2f5cbf6ba3717629e0d4e07cc9afa3ce4282b978
Danielyan86 <516495459@qq.com>
2026-02-08 17:07:49 +1300

    Optimize mySqrt function by adjusting the right pointer initialization and correcting the return value for improved accuracy in square root calculation.

--- TwoPointers/69. Sqrt(x).py
+++ TwoPointers/69. Sqrt(x).py
@@ -2,7 +2,7 @@ class Solution:
     def mySqrt(self, x: int) -> int:
         if x <= 1:
             return x
-        l, r = 0, x
+        l, r = 0, x // 2 + 1
         while l <= r:
             mid = l + (r - l) // 2
             if mid * mid == x:
@@ -11,7 +11,7 @@ class Solution:
                 r = mid - 1
             else:
                 l = mid + 1
-        return l + 1
+        return r
 
 
 if __name__ == "__main__":
